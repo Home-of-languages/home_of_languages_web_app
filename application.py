ed59bb64ded06e85c5a834a1c013be33d25ccb4a
Aleksandr S. Nikitiuk <31177834+AleksandrNikitiuk@users.noreply.github.com>
2022-04-05 12:12:12 +0500

    Right back usd)

--- application.py
+++ application.py
@@ -4,9 +4,9 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash, usd
+from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup
+from helpers import apology, login_required, lookup, usd
 
 # Configure application
 application = Flask(__name__)
